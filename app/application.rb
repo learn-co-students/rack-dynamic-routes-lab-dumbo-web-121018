1a3fcad09133c3622d710ee3b85f404c126ba1e5
Joshua Alphonse <joshalphonse@gmail.com>
2019-01-07 14:57:51 -0500

    Done.

---
+++ app/application.rb
@@ -0,0 +1,53 @@
+#require 'pry'
+class Application
+#  @@items =[Item.new("Figs",3.42),Item.new("Pears",0.99)]
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      #binding.pry
+      item_name = req.path.split("/").last
+
+      if item = @@items.find{|s| s.name == item_name}
+        resp.status = 200
+
+      resp.write item.price
+    else
+      resp.status = 400
+      resp.write "Item not found"
+    end
+
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+
+    resp.finish
+  end
+end
+
+# class Application
+#   def call(env)
+#     resp = Rack::Response.new
+#     req = Rack::Request.new(env)
+#
+#     if req.path.match(/items/)
+#       item_find = req.path.split("/").last
+#
+#       if item = @@items.find { |i| i.name == item_find }
+#         resp.status = 200
+#         resp.write item.price
+#       else
+#         resp.status = 400
+#         resp.write "Item not found"
+#       end
+#
+#     else
+#       resp.status = 404
+#       resp.write "Route not found"
+#     end
+#     resp.finish
+#   end
+# end
--- app/item.rb
+++ app/item.rb
@@ -1,8 +1,14 @@
 class Item
+  @@all =[]
   attr_accessor :name, :price
+
+  def self.all
+    @@all
+  end
 
   def initialize(name,price)
     @name = name
     @price = price
+    @@all << self
   end
 end
